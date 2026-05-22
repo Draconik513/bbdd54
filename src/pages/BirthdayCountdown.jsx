f2febbbc4300c47900e2b2d94fa0cd98a6b9178f
Draconik513 <draconik514@gmail.com>
2026-05-22 11:59:20 +0800

    bar:

--- src/pages/BirthdayCountdown.jsx
+++ src/pages/BirthdayCountdown.jsx
@@ -14,7 +14,7 @@ const BirthdayCountdown = ({ isIOS }) => {
   const [replyMessage, setReplyMessage] = useState("");
   const navigate = useNavigate();
 
-  const targetDate = new Date("2026-05-13T00:00:00");
+  const targetDate = new Date("2026-06-13T00:00:00");
 
   useEffect(() => {
     const timer = setInterval(() => {
